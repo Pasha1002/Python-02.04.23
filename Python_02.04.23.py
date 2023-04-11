c53e55c6d04be342eb26943d31eab45b5985f13e
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-11 12:26:44 +0300

    tusk dune

--- Python_02.04.23.py
+++ Python_02.04.23.py
@@ -1,1 +1,15 @@
 
+
+try:
+
+    string = input("Enter a string: ")
+    word = input("Enter a word: ")
+    count = 0
+    for i in range(len(string)):
+        if string[i:i+len(word)] == word:
+            count += 1
+    print("Тumber of searched words",word,"in a row",count)
+
+
+except Exception as a:
+    print(a)
