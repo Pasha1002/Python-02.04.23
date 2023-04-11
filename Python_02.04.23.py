313701cae80c5b6896824b7d6f37e7fae03223e9
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-11 12:15:34 +0300

    tusk dune

--- Python_02.04.23.py
+++ Python_02.04.23.py
@@ -1,1 +1,13 @@
 
+try:
+
+    string = input('Enter string:  ')
+    symbol = input(' Enter the symbol you are looking for:  ')
+    count = 0
+    for i in string:
+        if i == symbol:
+            count +=1
+    print(count)
+
+except Exception as a:
+    print(a)
