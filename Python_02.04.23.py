6f51305c002cd73f7db38ae217d7bf9edadd7908
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-11 12:11:15 +0300

    tusk dune

--- Python_02.04.23.py
+++ Python_02.04.23.py
@@ -1,1 +1,16 @@
 
+try:
+    string = input('Enter string:  ')
+    symbol = 0
+    digits = 0
+    for i in string:
+        if i.isalpha():
+            symbol +=1
+        elif i.isdigit():
+            digits +=1
+        else:
+            print('bl')
+    print('Symbols in string: ', symbol)
+    print('Numbers in string: ', digits)
+except Exception as a:
+    print(a)
