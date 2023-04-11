1d6b7b71d9e5e62583dafb42388eb1bcca5ea1b7
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-11 12:30:59 +0300

    tusk dune

--- Python_02.04.23.py
+++ Python_02.04.23.py
@@ -1,1 +1,13 @@
 
+
+try:
+
+    string = input('Enter string:  ')
+    search_word = input('Enter word what looking for:  ')
+    word_for_change = input('Enter replacement word:  ')
+    print(string.replace(search_word,word_for_change))
+
+
+
+except Exception as a:
+    print(a)
